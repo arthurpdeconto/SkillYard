5062238adca6c954a2e82d078f8d937cb403d2aa
arthurpdeconto <arthurdeconto04@gmail.com>
2025-10-29 01:36:34 -0300

    Espelha tipos do client Prisma gerado

--- types/prisma.d.ts
+++ types/prisma.d.ts
@@ -1,3 +1,7 @@
 declare module "@prisma/client" {
   export * from "@prisma/client/scripts/default-index";
 }
+
+declare module ".prisma/client/default" {
+  export * from "@prisma/client/scripts/default-index";
+}
